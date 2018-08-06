7904c31af6aee4fee48e6d6ad5805aeeeb0cf3b5
shivam5th <mr.rajeshdeb@gmail.com>
2018-08-06 18:41:54 +0530

    Number to Octal Convert

---
+++ c/c3_Loop/exercise/B/h.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n, oct[10], quo, i=0, j=-1;
+    
+    printf("Enter any number ");
+    scanf("%d",&n);
+    
+    quo=n;
+    while(quo!=0)
+    {
+        oct[i]=quo%8;
+        quo=quo/8;
+        i++;
+        j++;
+    }
+
+    printf("\nOctal Equilvalent Is\n\n");
+    
+    for(;j>=0;j--)
+    {
+        printf("%d",oct[j]);
+    }
+}
