6914dbc36c312a01571efe6372f95cc849e28fec
Joy Prakash Sharma <droidlabour@gmail.com>
2018-08-17 12:08:40 +0530

    Fix indentation

--- c_revision/chap6/exercise/B/abc.c
+++ c_revision/chap6/exercise/B/abc.c
@@ -4,19 +4,18 @@ int main()
 {
 	int rows=4, i, j, number= 1, space;
 	
-	for(i=1; i <= rows; i++)
+	for(i=1; i<=rows; i++)
 	{
-		for(space=1; space <= rows-i; space++)
-            		printf("  ");
+		for(space=1; space<=rows-i; space++)
+			printf("  ");
 		
 		for(j=1; j <= i; ++j)
 		{
-            		printf("%5d", number);
+			printf("%5d", number);
 			++number;
 
 		}
-			printf("\n");
+		printf("\n");
     }
-
     return 0;
 }
